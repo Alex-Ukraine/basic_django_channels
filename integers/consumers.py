cf5760d71874ac5f17e4b7f17673d3b22cc5c979
Esaulov Oleksii <win.city18@gmail.com>
2021-07-18 19:46:37 +0300

    Initial commit

---
+++ integers/consumers.py
@@ -0,0 +1,13 @@
+from random import randint
+from time import sleep
+from channels.generic.websocket import WebsocketConsumer
+import json
+
+
+# handler of all requests
+class WSConsumer(WebsocketConsumer):
+    def connect(self):
+        self.accept()
+        for i in range(1000):
+            self.send(json.dumps({'message': randint(1, 100)}))
+            sleep(1)
